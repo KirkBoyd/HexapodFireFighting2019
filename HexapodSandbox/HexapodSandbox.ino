ca47c3a4e2421dc07315f77e7510eee526b5af1c
Kirk Boyd <40477730+KirkBoyd@users.noreply.github.com>
2019-03-03 16:14:08 -0500

    Update HexapodSandbox.ino
    add more ports

--- HexapodSandbox/HexapodSandbox.ino
+++ HexapodSandbox/HexapodSandbox.ino
@@ -12,12 +12,15 @@
 #define button2 7//
 #define button3 8
 #define button4 9
-#define button5 10
-#define versa 11
-#define mic1port 12
-#define mic2port 13
-#define flameSensor1 14
-#define flameSensor2 15
+#define button5 10//
+#define versa 11//port for versa valve solenoid
+#define mic1port 12//1st mic for start sequence
+#define mic2port 13 //2nd mic for start sequence
+#define flameSensor1 14 //port for 1st flame sensor
+#define flameSensor2 15 //2nd flame sensor port, for now unused
+#define sharp1port 16 //sharp sensor 1 port for navigation
+#define sharp2port 17 //sharp sensor 2 port for navigation
+#define vidPort 18 //for now unused port, but dedicated to the video detection for later
 #define CFR 1 //ID num for Coxa Front Right Dynamixel AX12-A Servo
 #define CMR 2 //ID num for Coxa Middle Right Dynamixel AX12-A Servo
 #define CRR 3 //ID num for Coxa Rear Right Dynamixel AX12-A Servo
