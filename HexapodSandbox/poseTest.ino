3ce8a63695d8bfdd843039977a42eaefdeb3da1d
KirkBoyd <kirkland.boyd@trincoll.edu>
2019-03-19 11:37:06 +0000

    reconcile differences in local machine

--- HexapodSandbox/poseTest.ino
+++ HexapodSandbox/poseTest.ino
@@ -44,11 +44,11 @@ void walkStance(int t){
   SetPosition(12,680);
   delay(t);
 }
-void walkA(int t){//same as walkStance with raised CFL, CMR, CRL
+void walkA(int t){//raised CFL, CMR, CRL
   SetPosition(1,700);
   SetPosition(2,512);
-  SetPosition(3,590);
-  SetPosition(4,620);// changed from 450
+  SetPosition(3,470);//changed from 590 to 470
+  SetPosition(4,600);//changed from 450 to 600
   SetPosition(5,330);
   SetPosition(6,375);//
   SetPosition(7,680);//CFR down
@@ -59,13 +59,13 @@ void walkA(int t){//same as walkStance with raised CFL, CMR, CRL
   SetPosition(12,470);//CFL up
   delay(t);
 }
-void walkB(int t){//
+void walkB(int t){//2-4-6 fwd (down), 1-3-5 back (down)
   SetPosition(1,680);
   SetPosition(2,330);
-  SetPosition(3,590);
+  SetPosition(3,470);//changed from 590 to 470
   SetPosition(4,690);
   SetPosition(5,330);
-  SetPosition(6,600);
+  SetPosition(6,500);// changed from 600 to 500
   SetPosition(7,680);
   SetPosition(8,680);
   SetPosition(9,680);
@@ -74,11 +74,11 @@ void walkB(int t){//
   SetPosition(12,680);
   delay(t);
 }
-void walkC(int t){//1 3 5 fwd 2 4 6 back 
-  SetPosition(1,650);
+void walkC(int t){//1-3-5 back (up), 2-4-6 back (down)
+  SetPosition(1,450);//changed from 650 to 450
   SetPosition(2,680);
   SetPosition(3,375);
-  SetPosition(4,450);
+  SetPosition(4,570);//changed from 450 to 570
   SetPosition(5,330);
   SetPosition(6,330);
   SetPosition(7,470);
@@ -89,11 +89,11 @@ void walkC(int t){//1 3 5 fwd 2 4 6 back
   SetPosition(12,680);
   delay(t);
 }
-void walkD(int t){
+void walkD(int t){//1-3-5 fwd (down), 2-4-6 back (down)
   SetPosition(1,450);
   SetPosition(2,680);
   SetPosition(3,330);
-  SetPosition(4,450);//changed from 450
+  SetPosition(4,450);
   SetPosition(5,680);
   SetPosition(6,330);
   SetPosition(7,680);
@@ -104,10 +104,10 @@ void walkD(int t){
   SetPosition(12,680);
   delay(t);
 }
-void walkE(int t){
+void walkE(int t){//1-3-5 back (down), 2-4-6 back (up)
   SetPosition(1,680);
-  SetPosition(2,680);
-  SetPosition(3,590);
+  SetPosition(2,470);// changed from 680
+  SetPosition(3,470);//changed from 590
   SetPosition(4,590);
   SetPosition(5,330);
   SetPosition(6,330);
