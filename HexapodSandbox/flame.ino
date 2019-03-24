e4bd9164d1ba79be2bfbfb3d1b1a08e90fa73273
KirkBoyd <kirkland.boyd@trincoll.edu>
2019-03-24 18:33:53 +0000

    fix: update comments for firstFlameCheck

--- HexapodSandbox/flame.ino
+++ HexapodSandbox/flame.ino
@@ -2,13 +2,13 @@
 #define flameSmallMax 0  //closest a small IR phototransistor should be to extinguish the flame
   /** NOTE ^^^ for above values, high == far away. low == very close. **/
 #define uvTronMin 0 //smallest value that we can say uvtron has seen the flame
-#define uvTronMax 1023  //chosen value for when we can stop checking with uvtron and move to IR phototransistors for aiming
+#define uvTronMax 1023  //oversaturated value for uvTron
 
 bool firstFlameCheck(){
   if(analogRead(uvTronPort) >= uvTronMin && !flameSeen){//if the flame has not been detected yet, and the readings show that a flame is now present, store the fact that a flame has been seen.
     flameSeen = true;
   }
-  if(analogRead(uvTronPort) <= uvTronMax){
+  if(analogRead(uvTronPort) <= uvTronMax){//if the flame is oversaturating the uvTron, we can stop using it and aim with the IR phototransistors
     
   }
 }
