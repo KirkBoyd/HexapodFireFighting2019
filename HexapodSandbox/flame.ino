7313723fe084c8b77bbea5a688754c67c03fada1
KirkBoyd <kirkland.boyd@trincoll.edu>
2019-03-24 18:31:44 +0000

    feat: add first if statement and change name in function firstFlame- -> firstFlameCheck()

--- HexapodSandbox/flame.ino
+++ HexapodSandbox/flame.ino
@@ -1,10 +1,15 @@
 #define flameSmallMin 1023 //furthest a small IR phototransistor can be for consistent reading
 #define flameSmallMax 0  //closest a small IR phototransistor should be to extinguish the flame
   /** NOTE ^^^ for above values, high == far away. low == very close. **/
-#define uvtronMin 0 //smallest value that we can say uvtron has seen the flame
-#define uvtronMax 1023  //chosen value for when we can stop checking with uvtron and move to IR phototransistors for aiming
+#define uvTronMin 0 //smallest value that we can say uvtron has seen the flame
+#define uvTronMax 1023  //chosen value for when we can stop checking with uvtron and move to IR phototransistors for aiming
 
-bool firstFlame(){
-  if(digitalRead(u
+bool firstFlameCheck(){
+  if(analogRead(uvTronPort) >= uvTronMin && !flameSeen){//if the flame has not been detected yet, and the readings show that a flame is now present, store the fact that a flame has been seen.
+    flameSeen = true;
+  }
+  if(analogRead(uvTronPort) <= uvTronMax){
+    
+  }
 }
 
