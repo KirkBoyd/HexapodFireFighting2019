c7abe6db18bb83dbae8984392b1939d8d9936922
KirkBoyd <kirkland.boyd@trincoll.edu>
2019-03-24 18:51:03 +0000

    feat: add more logic to firstFlameCheck. This still needs some checking / work.

--- HexapodSandbox/flame.ino
+++ HexapodSandbox/flame.ino
@@ -1,6 +1,7 @@
 #define flameSmallMin 1023 //furthest a small IR phototransistor can be for consistent reading
 #define flameSmallMax 0  //closest a small IR phototransistor should be to extinguish the flame
-  /** NOTE ^^^ for above values, high == far away. low == very close. **/
+/** NOTE ^^^ for above values, high == far away. low == very close. **/
+
 #define uvTronMin 0 //smallest value that we can say uvtron has seen the flame
 #define uvTronMax 1023  //oversaturated value for uvTron
 
@@ -9,7 +10,11 @@ bool firstFlameCheck(){
     flameSeen = true;
   }
   if(analogRead(uvTronPort) <= uvTronMax){//if the flame is oversaturating the uvTron, we can stop using it and aim with the IR phototransistors
-    
+    uvTronDone = true;
   }
+  if(flameSeen && analogRead(uvTronPort)>= uvTronMin && !uvTronDone){ //if flame was spotted, but still too far for phototransistors, return true
+    return true;
+  }
+  else{return false;}
 }
 
