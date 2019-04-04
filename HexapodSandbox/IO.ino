188898f59d2741946094183c25bdb1c8ef541910
KirkBoyd <kirkland.boyd@trincoll.edu>
2019-04-04 19:17:44 -0400

    fix: move content of LED.ino to IO.ino. Delete LED.ino

--- HexapodSandbox/IO.ino
+++ HexapodSandbox/IO.ino
@@ -39,4 +39,21 @@ void lightCheck(){
     videoLED(false);
   }
 }
+/*************************LED************************************/
+void soundLED(boolean state){ // turns on blue LED when passed 'true'
+  if (state){digitalWrite (soundLEDport, HIGH);}
+  else{digitalWrite (soundLEDport, LOW);}
+}
+void flameLED(boolean state){ // turns on red LED when passed 'true'
+  if (state){digitalWrite (flameLEDport, HIGH);}
+  else{digitalWrite (flameLEDport, LOW);}
+}
+void videoLED(boolean state){ // turns on green LED when passed 'true'
+  if (state){digitalWrite (videoLEDport, HIGH);}
+  else{digitalWrite (videoLEDport, LOW);}
+}
+void runningLED(boolean state){ // turns on white LED when passed 'true'
+  if (state){digitalWrite(runningLEDport, HIGH);}
+  else{digitalWrite(runningLEDport, LOW);}
+}
 
