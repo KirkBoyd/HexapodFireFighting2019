acb9d083fd6c18e3ed0fe4435e7c77cca6b3105b
KirkBoyd <kirkland.boyd@trincoll.edu>
2019-04-12 21:03:10 -0400

    fix: change sharp vals and logic

--- HexapodSandbox/nav.ino
+++ HexapodSandbox/nav.ino
@@ -2,7 +2,7 @@
 #define sharp1max 230//updated 04/11/19 9pm
 #define sharp1a  285//(plusminus 7) closest it can be to the wall (on it's left)while parallel
 #define sharp1b 632//(plusminus 10) closest it could ever be to the wall, sharp 1 right in front
-#define sharp1c 250//(delta )almost too close to the wall on the left
+#define sharp1c 230//(delta )almost too close to the wall on the left
 #define sharp1d 120//(delta )almost too far from the wall on the left
 #define sharp1e 149//good distance from wall on left
 #define sharp1delta 9//average variance of sharp 1
@@ -17,8 +17,16 @@
 #define sharp4max 500//
 #define sharp4nothingThereLo 110
 #define sharp4nothingThereHi 155
+int val1;
+int val2;
+int val3;
+int val4;
 
 void navigate(){//use the sharp sensors to search the maze by avoiding walls 
+  val1 = s1();
+  val2 = s2();
+  val3 = s3();
+  val4 = s4();
   if(s3()>= sharp3max){//if something is too close in front, turn right
     turn90R();
   }
@@ -31,6 +39,22 @@ void navigate(){//use the sharp sensors to search the maze by avoiding walls
   }
   else if(s1()<=sharp1d){
     turnL();
+  }
+  //the if statement below basically checks for the specific condition in the top left room where the wall ends
+  else if((55<s2()&&s2()<155)&&(105<s3()&&s3()<165)&&(300<s4()&&s4()<600)){//when approaching a wall that ends on the left side
+    blinkLED(runningLEDport);
+    while(s3()<210){
+      if(s1()>=sharp1c){turnR();}
+      else if(s4()>=450){turnR();}
+      else{fwd();fwd();}
+    }
+    turn90L();
+    fwd();
+    fwd();
+    turn90L();
+    while(s4()<290){
+      fwd();
+    }    
   }
   else{
     fwd();
